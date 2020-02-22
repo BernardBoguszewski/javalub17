b199efddfffac6b40b952857657d64f939e940ae
milo <milo99@vp.pl>
2020-02-22 14:59:03 +0100

    wymaganie 2

--- src/test/java/com/example/demo/GreetingsTest.java
+++ src/test/java/com/example/demo/GreetingsTest.java
@@ -17,7 +17,20 @@ class GreetingsTest {
 
         //then
         assertEquals("Witaj, Adam", result);
-
-
     }
+
+    @Test
+    void testShouldReturnGreetingForNull() {
+        // given
+        String name = null;
+        Greetings greetings = new Greetings();
+
+        // when
+        String result = greetings.greet(name);
+
+
+        // then
+        assertEquals("Witaj, mój przyjacielu", result);
+    }
+
 }
