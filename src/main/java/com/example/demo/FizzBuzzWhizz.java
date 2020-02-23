0d4709341967fafb18c78207407156be9f4120b2
Agata Chmielewska <agata.chmielewska94@gmail.com>
2020-02-23 11:43:17 +0100

    Zadanie 2 wymaganie 3 i 4

--- src/main/java/com/example/demo/FizzBuzzWhizz.java
+++ src/main/java/com/example/demo/FizzBuzzWhizz.java
@@ -4,8 +4,14 @@ public class FizzBuzzWhizz {
 
     public String fizzBuzzWhizz(Integer digit) {
         if (digit != null) {
-            if(digit % 3 == 0){
+            if(digit % 5 == 0 & digit % 3 == 0){
+                return "FizzBuzz";
+            }
+            else if(digit % 3 == 0){
                 return "Fizz";
+            }
+            else if(digit % 5 == 0){
+                return "Buzz";
             }
         }
         return "Nie dla null!";
--- src/test/java/com/example/demo/FizzBuzzWhizzTest.java
+++ src/test/java/com/example/demo/FizzBuzzWhizzTest.java
@@ -22,7 +22,7 @@ class FizzBuzzWhizzTest {
     @Test
     void shouldReturnResponseForDigitDivisibleInto3(){
         //given
-        Integer digit = 15;
+        Integer digit = 9;
         FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
 
         //when
@@ -32,4 +32,29 @@ class FizzBuzzWhizzTest {
         assertEquals("Fizz", result);
     }
 
+    @Test
+    void shouldReturnResponseForDigitDivisibleInto5(){
+        //given
+        Integer digit = 10;
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+
+        //when
+        String result = fizzBuzzWhizz.fizzBuzzWhizz(digit);
+
+        //then
+        assertEquals("Buzz", result);
+    }
+
+    @Test
+    void shouldReturnResponseForDigitDivisibleInto3And5(){
+        //given
+        Integer digit = 15;
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+
+        //when
+        String result = fizzBuzzWhizz.fizzBuzzWhizz(digit);
+
+        //then
+        assertEquals("FizzBuzz", result);
+    }
 }
