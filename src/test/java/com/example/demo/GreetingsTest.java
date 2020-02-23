4ab9b9ee4347d3a6a70650d4248c251bae43dbcc
adamo <asiestrzykowski@wp.pl>
2020-02-23 10:47:31 +0100

    Wymaganie 2

--- src/test/java/com/example/demo/GreetingsTest.java
+++ src/test/java/com/example/demo/GreetingsTest.java
@@ -20,4 +20,18 @@ class GreetingsTest {
 
 
     }
+    @Test
+    void testShouldReturnGreetingWithNull() {
+        //given
+        String name = null;
+        Greetings greetings = new Greetings();
+
+        //when
+        String result = greetings.greet(name);
+
+        //then
+        assertEquals("Witaj, mój przyjacielu", result);
+
+
+    }
 }
