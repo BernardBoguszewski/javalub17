160eb25fa3ac3354f2998a3d834a64502e3ac7bc
bartosz <bartoszklocek1985@gmail.com>
2020-02-23 09:53:36 +0100

    wymaganie2 Zabezpiecz metodę przed nullami. Gdy w parametrze zostanie przekazany null, wtedy metoda powinna zwrócić „Witaj, mój przyjacielu”

--- src/main/java/com/example/demo/Greetings.java
+++ src/main/java/com/example/demo/Greetings.java
@@ -3,6 +3,10 @@ package com.example.demo;
 public class Greetings {
 
     public String greet(String name) {
+
+        if (name==null){
+            return "Witaj, mó przyjacielu";
+        }
         return "Witaj, " + name;
     }
 }
