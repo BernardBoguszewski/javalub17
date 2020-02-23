16cb30ce0e172d077be88ab73742a617f6548261
milo <milo99@vp.pl>
2020-02-23 11:06:33 +0100

    zadanie 3 wszystkie wymagania

--- src/main/java/com/example/demo/FizzBuzzWhizz.java
+++ src/main/java/com/example/demo/FizzBuzzWhizz.java
@@ -6,7 +6,40 @@ public class FizzBuzzWhizz {
         if (digit == null) {
             return "Nie dla null!";
         } else {
-            return null;
+            if (digit == 3) {
+                return "FizzBuzz";
+            } else {
+                if (digit == 5) {
+                    return "BuzzWhizz";
+                } else {
+                    if (digit % 3 == 0) {
+                        if (digit % 5 == 0) {
+                            return "FizzBuzz";
+                        } else {
+                            return "Fizz";
+                        }
+                    } else {
+                        if (digit % 5 == 0) {
+                            return "Buzz";
+                        } else {
+                            if (isPrime(digit)) {
+                                return "Whizz";
+                            } else {
+                                return null;
+                            }
+                        }
+                    }
+                }
+            }
         }
+    }
+
+    private static boolean isPrime(Integer digit) {
+        for (int i = 2; i < digit; i++) {
+            if (digit % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/com/example/demo/FizzBuzzWhizzTest.java
+++ src/test/java/com/example/demo/FizzBuzzWhizzTest.java
@@ -19,4 +19,82 @@ class FizzBuzzWhizzTest {
         assertEquals("Nie dla null!", result);
     }
 
+    @Test
+    void testShouldReturnFizzForNumberDivisibleByThree() {
+        // given
+        Integer digit = 6;
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+
+        // when
+        String result = fizzBuzzWhizz.fizzBuzzWhizz(digit);
+
+        // then
+        assertEquals("Fizz", result);
+    }
+
+    @Test
+    void testShouldReturnBuzzForNumberDivisibleByFive() {
+        // given
+        Integer digit = 10;
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+
+        // when
+        String result = fizzBuzzWhizz.fizzBuzzWhizz(digit);
+
+        // then
+        assertEquals("Buzz", result);
+    }
+
+    @Test
+    void testShouldReturnFizzBuzzForNumberDivisibleByThreeAndFive() {
+        // given
+        Integer digit = 15;
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+
+        // when
+        String result = fizzBuzzWhizz.fizzBuzzWhizz(digit);
+
+        // then
+        assertEquals("FizzBuzz", result);
+    }
+
+    @Test
+    void testShouldReturnWhizzForPrimeNumbers() {
+        // given
+        Integer digit = 7;
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+
+        // when
+        String result = fizzBuzzWhizz.fizzBuzzWhizz(digit);
+
+        // then
+        assertEquals("Whizz", result);
+    }
+
+    @Test
+    void testShouldReturnFizzWhizzForThree() {
+        // given
+        Integer digit = 3;
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+
+        // when
+        String result = fizzBuzzWhizz.fizzBuzzWhizz(digit);
+
+        // then
+        assertEquals("FizzBuzz", result);
+    }
+
+    @Test
+    void testShoultReturnBuzzWhizzForFive() {
+        // given
+        Integer digit = 5;
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+
+        // when
+        String result = fizzBuzzWhizz.fizzBuzzWhizz(digit);
+
+        // then
+        assertEquals("BuzzWhizz", result);
+    }
+
 }
